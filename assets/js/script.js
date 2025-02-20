a732a23e35a9106c0b4a23bec293a352f966ce4d
Bibhu Prasad <bibhup.behera@nettantra.net>
2025-02-20 22:56:43 +0530

    update js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll(".dsp-read-more").forEach(button => {
         button.addEventListener("click", function (e) {
-            if (this.getAttribute("href") === "#") {
+            let contentDiv = this.closest(".dsp-preview");
+            let fullContent = contentDiv?.getAttribute("data-full-content");
+            if (fullContent) {
                 e.preventDefault();
-                let contentDiv = this.closest(".dsp-preview");
-                let fullContent = contentDiv.getAttribute("data-full-content");
-                contentDiv.innerHTML = fullContent;
+                let effect = this.getAttribute("data-effect");
+
+                if (effect === "slide") {
+                    contentDiv.innerHTML = fullContent;
+                    contentDiv.style.maxHeight = contentDiv.scrollHeight + "px";
+                    contentDiv.style.transition = "max-height 0.5s ease-in-out";
+                } else if (effect === "fade") {
+                    contentDiv.style.opacity = 0;
+                    setTimeout(() => {
+                        contentDiv.innerHTML = fullContent;
+                        contentDiv.style.opacity = 1;
+                        contentDiv.style.transition = "opacity 0.5s ease-in-out";
+                    }, 200);
+                } else {
+                    contentDiv.innerHTML = fullContent;
+                }
             }
         });
     });
